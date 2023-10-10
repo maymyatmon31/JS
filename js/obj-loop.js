b83b0af2bb9e779ccc163f569b6b76443f449c7d
maymyatmon31 <mmm.maymyatmon31@gmail.com>
2023-10-10 11:12:37 +0630

    for in, for of

---
+++ js/obj-loop.js
@@ -0,0 +1,11 @@
+const fruits = ["apple", "orange", "mango", "rambutan", "pomelo", "jack fruit", "damson"];
+
+// for(let i=0 ; i<fruits.length; i++) {
+//     console.log(i, fruits[i]);
+// }
+for(fruit of fruits) {
+    console.log(fruit);
+}
+for(index in fruits){
+    console.log(index);
+}
